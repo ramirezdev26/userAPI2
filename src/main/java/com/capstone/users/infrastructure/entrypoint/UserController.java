8676080de331ccc1728139d93a96d4a7647c8d11
santiago.ramirez <santiago.ramirezc@sofka.com.co>
2024-09-09 13:36:35 -0500

    [Feat] Protection for routes
    Adding dependencies, implementation for private and public routes

--- src/main/java/com/capstone/users/infrastructure/entrypoint/UserController.java
+++ src/main/java/com/capstone/users/infrastructure/entrypoint/UserController.java
@@ -1,6 +1,7 @@
 package com.capstone.users.infrastructure.entrypoint;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/users")
 @AllArgsConstructor
 public class UserController {
+
+    @PostMapping(value = "/register")
+    public String register() {
+        return "Register from secure endpoint";
+    }
+
 }
