dce89546a1e04b0bf36018e68c03d59016d304dc
OwenBueno <owen.i.sanchez.r@gmail.com>
2024-09-13 12:36:42 -0600

    feat: Implementation of route protection. This commit implement security for wrong request to our api: - new dependencies. - new config for routes - default routes.

--- src/main/java/com/capstone/users/infrastructure/entrypoint/UserController.java
+++ src/main/java/com/capstone/users/infrastructure/entrypoint/UserController.java
@@ -1,11 +1,17 @@
 package com.capstone.users.infrastructure.entrypoint;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("api/v1/users")
 @AllArgsConstructor
 public class UserController {
+
+    @PostMapping(value = "/register")
+    public String register() {
+        return "Register from secure endpoint";
+    }
+
 }
