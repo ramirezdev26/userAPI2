2b2089b879a1a74db6f4c6ad629344e12892a4a4
Miguel Angel Barrios Perez <Miguel.Barrios@jala.university>
2024-09-16 15:21:50 -0500

    protected register route removal

--- src/main/java/com/capstone/users/infrastructure/entrypoint/UserController.java
+++ src/main/java/com/capstone/users/infrastructure/entrypoint/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +25,6 @@ public class UserController {
 
     private final UserService userService;
     private final  PasswordEncoder passwordEncoder;
-    @PostMapping(value = "/register")
-    public ResponseEntity<User> register(@RequestBody User user) {
-        userService.save(user);
-        return ResponseEntity.ok(user);
-    }
 
     @Operation(summary = "User Update")
     @PutMapping(value = "/update/{id}")
